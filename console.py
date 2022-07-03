989d5038b3474268f38f0e117e6bd8bd8a70948c
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-03 18:03:11 -0300

    commit

---
+++ console.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a program called console.py that contains\
+the entry point of the command interpreter"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """entry point of the command interpreter"""
+    prompt = "(hbnb) "
