a1b3c1b40ddfd0098c73e8992ddc86cbdde2d834
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-04 17:19:15 -0700

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -15,9 +15,9 @@ class BaseModel:
                 if key == "id":
                     self.id = value
                 elif key == "created_at":
-                    self.created_at = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
+                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == "updated_at":
-                    self.updated_at = value
+                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
