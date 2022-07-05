777b618445bd8741cd80c6731d5b555f329a4af8
RicardoDanta <uruguaydenos@gmail.com>
2022-07-05 16:23:26 -0700

    JSON Dump

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,7 +31,8 @@ class FileStorage:
         for key, value in new_dict.items():
             new_dict[key] = value.to_dict()
         with open(self.__file.path, w) as f:
-            return f.write(json.dump(self.__objects))
+            self.__objects = json.dump(f)
+            f.write
 
     def reload(self):
         """Deserialization"""
