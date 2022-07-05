0477cd4176e1b4b6a292784d7b975f07b66068bb
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-05 11:37:57 -0300

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,10 @@ import uuid
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
-    
+
     def __init__(self, *args, **kwargs):
         """initializing"""
+        is_format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -20,9 +21,9 @@ class BaseModel:
                 elif key == "id":
                     self.id = value
                 elif key == "created_at":
-                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    self.created_at = datetime.strptime(value, is_format)
                 elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    self.updated_at = datetime.strptime(value, is_format)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
