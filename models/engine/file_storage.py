02f60fd6ef2f34c7e18261eb7d79a9a8f24b60b8
RicardoDanta <uruguaydenos@gmail.com>
2022-07-06 06:42:19 -0700

    FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,7 +31,7 @@ class FileStorage:
         for key, value in new_dict.items():
             new_dict[key] = value.to_dict()
         with open(self.__file.path, w) as f:
-            return f.write(json.dumps(self.__objects, default=str))
+            return f.write(json.dumps(self.__objects)
 
     def reload(self):
         """Deserialization"""
