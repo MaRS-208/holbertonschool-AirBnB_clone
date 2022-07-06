5271da9b0fb0756dbcc723636a924ee7dba0be9b
RicardoDanta <uruguaydenos@gmail.com>
2022-07-06 05:37:22 -0700

    Datetime

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
     def save(self):
         """update the public instance attribute"""
         self.updated_at = datetime.now()
-        self.updated_at = datetime.strptime('%Y-%m-%dT%H:%M:%S.%f')
+        updated_at = datetime.strptime('%Y-%m-%dT%H:%M:%S.%f')
         storage.save()
 
     def to_dict(self):
